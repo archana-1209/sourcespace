76763e1b6b5998b7609eb48c4f1acdf17a2863b8
Gaurav Sharma <gaurav91297@gmail.com>
2020-12-09 05:18:35 +0530

    Removed Relative imports in authentication.views and instead of importing whole views module at once, imported single view at a time

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,38 +1,42 @@
 from django.urls import path
 
-from . import views
-
-app_name = "authentication"
+from authentication.views import (
+    AuthLoginView, AuthLogoutView, 
+    AuthRegisterView,
+    AuthPasswordResetView,
+    PasswordChangeView,
+    ProfileView,
+    )
 
 urlpatterns = [
     path(
         "account/login/",
-        views.AuthLoginView.as_view(),
+        AuthLoginView.as_view(),
         name="login",
     ),
     path(
         "account/logout/",
-        views.AuthLogoutView.as_view(),
+        AuthLogoutView.as_view(),
         name="logout",
     ),
     path(
         "account/register/",
-        views.AuthRegisterView.as_view(),
+        AuthRegisterView.as_view(),
         name="register",
     ),
     path(
         "account/change_password/",
-        views.PasswordChangeView.as_view(),
+        PasswordChangeView.as_view(),
         name="change_password",
     ),
     path(
         "account/profile/",
-        views.ProfileView.as_view(),
+        ProfileView.as_view(),
         name="profile",
     ),
     path(
         "account/password_reset/",
-        views.AuthPasswordResetView.as_view(),
+        AuthPasswordResetView.as_view(),
         name="password_reset",
     ),
 ]
