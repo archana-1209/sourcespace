20bdce4527ec29b6516567132200cd5b42f55140
mohitCodepy <mohit1511199@gmail.com>
2020-12-10 15:21:36 +0530

    Signout function added

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import View
+from django.contrib.auth import logout
 
-# Create your views here.
+
+
+
+
+# Implementation of logout using CBV
+
+class AuthLogoutView(View):
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        return redirect('login')
+        
